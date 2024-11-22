36e5472fafe7deb7236e72aef365bc068433fe98
bitcamp <wkdxorgk@gmail.com>
2024-11-22 11:15:52 +0900

    123

--- src/main/java/com/petnet/domain/topic/vo/Topic.java
+++ src/main/java/com/petnet/domain/topic/vo/Topic.java
@@ -26,5 +26,10 @@ public class Topic {
     private Timestamp updateDate;       // UPDATE_DATE
     private String files;  // FILES (JSON)
     private Boolean isDownloadable;     // IS_DOWNLOADABLE
-//이건 시험 한번 하보는거
+//이건 시험 한번 하보는
+//123
+
+
+
+    /// /12321312312312312321
 }
